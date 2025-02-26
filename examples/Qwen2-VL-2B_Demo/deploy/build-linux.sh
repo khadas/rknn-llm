b4a9cd292074425aaf5fad37e2178dfb81ee2f14
Jacobe Zang <jacobe.zang@wesion.com>
2025-02-26 17:50:19 +0800

    qwen2: modify build path
    Signed-off-by: Jacobe Zang <jacobe.zang@wesion.com>

--- examples/Qwen2-VL-2B_Demo/deploy/build-linux.sh
+++ examples/Qwen2-VL-2B_Demo/deploy/build-linux.sh
@@ -1,12 +1,35 @@
-rm -rf build
-mkdir build && cd build
+#!/bin/bash
+# Debug / Release / RelWithDebInfo
+if [[ -z ${BUILD_TYPE} ]];then
+    BUILD_TYPE=Release
+fi
 
 GCC_COMPILER_PATH=aarch64-linux-gnu
-cmake .. -DCMAKE_CXX_COMPILER=${GCC_COMPILER}/bin/aarch64-none-linux-gnu-g++  \
-        -DCMAKE_C_COMPILER=${GCC_COMPILER}/bin/aarch64-none-linux-gnu-gcc \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_SYSTEM_NAME=Linux \
-        -DCMAKE_SYSTEM_PROCESSOR=aarch64 \
+C_COMPILER=${GCC_COMPILER_PATH}-gcc
+CXX_COMPILER=${GCC_COMPILER_PATH}-g++
+STRIP_COMPILER=${GCC_COMPILER_PATH}-strip
 
-make -j8
+TARGET_ARCH=aarch64
+TARGET_PLATFORM=linux
+if [[ -n ${TARGET_ARCH} ]];then
+TARGET_PLATFORM=${TARGET_PLATFORM}_${TARGET_ARCH}
+fi
+
+ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
+BUILD_DIR=${ROOT_PWD}/build/build_${TARGET_PLATFORM}_${BUILD_TYPE}
+
+if [[ ! -d "${BUILD_DIR}" ]]; then
+  mkdir -p ${BUILD_DIR}
+fi
+
+cd ${BUILD_DIR}
+cmake ../.. \
+    -DCMAKE_SYSTEM_PROCESSOR=${TARGET_ARCH} \
+    -DCMAKE_SYSTEM_NAME=Linux \
+    -DCMAKE_C_COMPILER=${C_COMPILER} \
+    -DCMAKE_CXX_COMPILER=${CXX_COMPILER} \
+    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+
+make -j4
 make install
